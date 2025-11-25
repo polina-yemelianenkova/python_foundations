4bc8640424a87a8ac503347bbc851ce2bb059733
Polina Yemelianenkova <145330202+polina-yemelianenkova@users.noreply.github.com>
2025-11-25 19:04:48 +0000

    added file 6

---
+++ Chapter 7/6.py
@@ -0,0 +1,9 @@
+import random
+
+def dice():
+    list = []
+    number = int(input("Please enter the number of times: "))
+    len(list) = number
+    for i in list:
+        number = random.rand
+    
